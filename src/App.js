7550f33afd71d02507611773d7e1e2bebd987a06
fahadshoukat <fraza6970@gmail.com>
2022-03-28 23:17:51 +0500

    add bootstrap and card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
 
 const App = () => {
-  return 
+  return (
+    <div className="container-fluid">
+      <div class="row">
+        <div class="col-sm-8 col-md-10 m-auto">
+          <div class="card">
+            <div class="card-body text-center">
+              <h1 class="card-title">Login</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  
+  )
 }
 
 export default App;
