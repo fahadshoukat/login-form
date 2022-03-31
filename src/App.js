5ffeecb6f37a2665e8989b353e7d76f1fd8b9f85
fahadshoukat <fraza6970@gmail.com>
2022-03-31 22:51:49 +0500

    add 2 inputs with icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
 import { FaRegUser } from "react-icons/fa";
-import { FaLock } from 'react-icons/fa'
+import { FaLock } from "react-icons/fa";
 const App = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-        <i class="bi bi-person"></i>
+    <div className="container border border-info">
+      <div className="row d-flex justify-content-center align-items-center vh-100">
+        <div className="col col-md-6 border border-danger bg-white p-4">
+          <h1 class="text-center">Login</h1>
+          <div class="input-group flex-nowrap">
+            <FaRegUser class="h3 self-align-center" />
+            <input
+              type="text"
+              class="form-control p-2 inputBorder"
+              placeholder="Username"
+            />
+          </div>
+          <div class="input-group flex-nowrap">
+            <FaLock class="h3 align-middle" />
+            <input
+              type="password"
+              class="form-control p-2 inputBorder"
+              placeholder="Password"
+            />
+          </div>
         </div>
       </div>
     </div>
