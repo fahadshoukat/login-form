1365234ab747982b1bb1cdaae806c333d6e72ce5
fahadshoukat <fraza6970@gmail.com>
2022-04-01 23:08:18 +0500

    precise import statement

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import "./App.css";
-import { FaRegUser } from "react-icons/fa";
-import { FaLock } from "react-icons/fa";
-import { FaFacebookSquare } from "react-icons/fa";
-import { FaTwitterSquare, FaGooglePlusSquare } from "react-icons/fa";
+import {
+  FaTwitterSquare,
+  FaGooglePlusSquare,
+  FaFacebookSquare,
+  FaLock,
+  FaRegUser,
+} from "react-icons/fa";
 const App = () => {
   return (
     <div className="container">
       <div className="row d-flex justify-content-center align-items-center vh-100">
         <div className="col col-md-6 col-lg-5 col-xl-4 bg-white p-5 ">
           <h1 class="text-center">Login</h1>
-            <label for="basic-url" class="form-label mt-4">
-              Username
-            </label>
-            <div class="input-group flex-nowrap">
-              <FaRegUser class="h3 self-align-center" />
-              <input
-                type="text"
-                class="form-control inputGroup-sizing-lg inputBorder"
-                placeholder="Username"
-              />
-            </div>
-            <label for="basic-url" class="form-label mt-4">
-              Password
-            </label>
-            <div class="input-group flex-nowrap">
-              <FaLock class="h3 align-middle" />
-              <input
-                type="password"
-                class="form-control inputGroup-sizing-lg inputBorder"
-                placeholder="Password"
-              />
-            </div>
+          <label for="basic-url" class="form-label mt-4">
+            Username
+          </label>
+          <div class="input-group flex-nowrap">
+            <FaRegUser class="h3 self-align-center" />
+            <input
+              type="text"
+              class="form-control inputGroup-sizing-lg inputBorder"
+              placeholder="Username"
+            />
+          </div>
+          <label for="basic-url" class="form-label mt-4">
+            Password
+          </label>
+          <div class="input-group flex-nowrap">
+            <FaLock class="h3 align-middle" />
+            <input
+              type="password"
+              class="form-control inputGroup-sizing-lg inputBorder"
+              placeholder="Password"
+            />
+          </div>
           <p class="text-end mt-3">
             <a href="#" class="text-decoration-none text-dark">
               Forgot password?
@@ -51,7 +54,10 @@ const App = () => {
           </div>
           <div class="text-center">
             <p class="mt-5">Or Sign Up Using</p>
-            <a href="#" class="text-decoration-none text-dark :hover-color-blue">
+            <a
+              href="#"
+              class="text-decoration-none text-dark :hover-color-blue"
+            >
               SIGN UP
             </a>
           </div>
