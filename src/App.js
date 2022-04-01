b76cc0a1367dc6f2f3de4c00b7b70f35aecfd19f
fahadshoukat <fraza6970@gmail.com>
2022-04-01 23:06:50 +0500

    add signup link section and style

--- src/App.js
+++ src/App.js
@@ -5,47 +5,55 @@ import { FaFacebookSquare } from "react-icons/fa";
 import { FaTwitterSquare, FaGooglePlusSquare } from "react-icons/fa";
 const App = () => {
   return (
-    <div className="container border border-info">
+    <div className="container">
       <div className="row d-flex justify-content-center align-items-center vh-100">
-        <div className="col col-md-6 col-lg-5 border border-danger bg-white p-4">
+        <div className="col col-md-6 col-lg-5 col-xl-4 bg-white p-5 ">
           <h1 class="text-center">Login</h1>
-          <label for="basic-url" class="form-label">
-            Username
-          </label>
-          <div class="input-group flex-nowrap">
-            <FaRegUser class="h3 self-align-center" />
-            <input
-              type="text"
-              class="form-control inputGroup-sizing-lg inputBorder"
-              placeholder="Username"
-            />
-          </div>
-          <label for="basic-url" class="form-label">
-            Password
-          </label>
-          <div class="input-group flex-nowrap">
-            <FaLock class="h3 align-middle"/>
-            <input
-              type="password"
-              class="form-control inputGroup-sizing-lg inputBorder"
-              placeholder="Password"
-            />
-          </div>
-          <p class="text-end">
+            <label for="basic-url" class="form-label mt-4">
+              Username
+            </label>
+            <div class="input-group flex-nowrap">
+              <FaRegUser class="h3 self-align-center" />
+              <input
+                type="text"
+                class="form-control inputGroup-sizing-lg inputBorder"
+                placeholder="Username"
+              />
+            </div>
+            <label for="basic-url" class="form-label mt-4">
+              Password
+            </label>
+            <div class="input-group flex-nowrap">
+              <FaLock class="h3 align-middle" />
+              <input
+                type="password"
+                class="form-control inputGroup-sizing-lg inputBorder"
+                placeholder="Password"
+              />
+            </div>
+          <p class="text-end mt-3">
             <a href="#" class="text-decoration-none text-dark">
               Forgot password?
             </a>
           </p>
           <div class="d-grid gap-2">
-            <button class="btn fw-bold text-white" type="button">
+            <button class="btn fw-bold text-white mt-3" type="button">
               LOGIN
             </button>
           </div>
-          <p class="text-center">Or Sign Up Using</p>
           <div class="text-center">
-          <FaFacebookSquare className="fbIcon" />
-          <FaTwitterSquare className="twIcon" />
-          <FaGooglePlusSquare className="gpIcon" />
+            <p class="mt-5">Or Sign Up Using</p>
+            <div>
+              <FaFacebookSquare className="fbIcon" />
+              <FaTwitterSquare className="twIcon" />
+              <FaGooglePlusSquare className="gpIcon" />
+            </div>
+          </div>
+          <div class="text-center">
+            <p class="mt-5">Or Sign Up Using</p>
+            <a href="#" class="text-decoration-none text-dark :hover-color-blue">
+              SIGN UP
+            </a>
           </div>
         </div>
       </div>
